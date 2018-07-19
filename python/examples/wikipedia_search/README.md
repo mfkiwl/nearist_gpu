8d301e5d384862ddd38384ae97104fe58073e2f1
Chris McCormick <chrisjmccormick@gmail.com>
2018-07-18 17:48:07 -0700

    Initial commit of live wikipedia search example

---
+++ python/examples/wikipedia_search/README.md
@@ -0,0 +1,65 @@
+# Wikipedia Article Similarity Search
+
+In this example, you specify the title of an article on Wikipedia, and the hardware performs a k-NN search to retrieve the ten most conceptually similar articles on Wikipedia. 
+
+We have applied Latent Semantic Indexing (LSI) to create vector representations for the roughly 4.2M articles on Wikipedia. We used the [wiki-sim-search](http://github.com/chrisjmccormick/wiki-sim-search/) project available on GitHub to generate this dataset.
+
+This example includes three components:
+1. `query_wikipedia_live.py` - This script uses the GpuClient to perform a single search.
+2. `wikipedia_knn_graph.py` - This script finds the 10 nearest neighbors for all vectors in the dataset.
+3. `query_wikipedia_graph.py` - This script uses the precomputed graph from the previous script to perform the search.
+
+## Pre-Requisites
+
+In order to run these examples, you will need the following dataset files downloaded to your local machine. The scripts expect them to be found in a subdirectory named `wiki_data`
+<table>
+  <tr>  <th>File</th>               <th>Size</th>    <th>Description</th>   </tr>
+ 
+  <tr>  <td><a href="">lsi_index_float32.h5</a></td>   <td>4.7 GB</td>   <td>LSI vectors for every article in Wikipedia. ~4.2 million rows with 300 features each.</td>    </tr>
+  
+  <tr>  <td><a href="https://drive.google.com/open?id=1wCg61RgNc0LbMjePSZUBUIxg1T46FujY">titles_to_id.pickle</a></td>   <td>130 MB</td>  <td>Dictionary for looking up the ID of an article with a given title.
+</td>    </tr>  
+</table>
+
+You'll also need:
+* The Nearist classes ('/nearist_gpu/python/src/') on your Python Path.
+* The API access key generated for your user account.
+* An active rental session and the IP address of your reserved Nearist server.
+    * _Make sure to update your copy of the example code with your API key and the provided IP address before running._
+
+## Running the examples
+
+Loading the dataset, both locally and on the remote server, is somewhat time consuming. After loading these files on your first run of the example, set the 'loaded' flag to true in the script, and it will skip the load steps for subsequent runs.
+
+You may run the script within your favorite Python IDE, or, run from a Python terminal with the command ```exec(open("query_wikipedia_live.py").read(), globals())```
+
+### Expected Output - query_wikipedia_live
+```
+Connecting to Nearist server...
+    Connection successful.
+
+Loading dataset vectors from remote server into GPU...
+Loading article titles...
+Retrieving local query vector...
+
+Finding most similar articles to "Water treatment"...
+
+                                                 Title    Distance
+                                                 =====    ========
+                            Industrial water treatment    0.025
+                                        Drinking water    0.042
+                                             Raw water    0.051
+                                       Reclaimed water    0.051
+                                    Water chlorination    0.056
+                                          Water bottle    0.056
+                                    Water purification    0.057
+                                 Groundwater pollution    0.060
+                                        Purified water    0.064
+                  Water issues in developing countries    0.064
+
+Timing breakdown:
+  Server Time: 83 ms
+     Overhead: 101 ms
+        Total: 184 ms
+```
+
---
+++ python/examples/wikipedia_search/query_wikipedia_live.py
@@ -0,0 +1,127 @@
+"""
+Purpose:
+
+This code returns the titles of the 10 most similar Wikipedia articles against a 
+given query wikipedia article title.
+
+
+
+Suggested use:
+
+Copy code into an IDE that keeps variables in memory (Spyder, Jupyter, etc.). This way,
+the datasets in memory and connection to Nearist server will persist while you 
+iteratively search new query terms and adjust parameters.
+
+
+
+Query term: 
+
+The name of a wikipedia article title. Titles are capitalized, and typically 
+each word of a multiword title is capitalizaed. Refer to wikipedia article titles online.
+
+"""
+
+from gpuclient import GpuClient
+import h5py
+import pickle
+
+# ==============================
+#     Parameters
+# ==============================
+
+# Specify the title of the Wikipedia article to use as the search query.
+#
+# Multi-word titles can specified with spaces, such as "Abraham Lincoln".
+# Capitalization should match the Wikipedia article title exactly. See the
+# examples below.
+
+#query_article = "Abraham Lincoln"
+#query_article = "Computer science"
+query_article = "Water treatment"
+
+# Connection parameters
+api_key = "apikey"
+nearist_port = 0
+nearist_ip = ""
+
+# Filepath parameters 
+path_on_nearist_server = "/nearist/Wikipedia/lsi_index_float32.h5"
+path_on_local_drive = "./wiki_data/lsi_index_float32.h5"
+path_to_titles = "./wiki_data/titles_to_id.pickle"
+
+
+# ==============================
+#     Establish connection
+# ==============================
+
+print('Connecting to Nearist server...')
+
+c = GpuClient()
+c.open(nearist_ip, nearist_port, api_key)
+
+print('    Connection successful.\n')
+
+# ==============================
+#       Load the dataset
+# ==============================
+# Set the '*_loaded' flags to 'True' after the first run of this script so that
+# you don't have to load anything again.
+remote_loaded = False
+if not remote_loaded:
+    print('Loading dataset vectors from remote server into GPU...')
+
+    # Load dataset into GPU memory.
+    c.load_dataset_file(file_name=path_on_nearist_server, dataset_name='lsi')
+    
+local_loaded = False
+if not local_loaded:    
+    print('Loading article titles...')
+    
+    # Load article titles
+    title_to_idx = pickle.load( open( path_to_titles, "rb" ) )
+    id_to_title = dict(zip(title_to_idx.values(), title_to_idx.keys()))
+    
+    # Open the local copy of the dataset file.
+    h5f = h5py.File(path_on_local_drive, 'r')
+
+# ==================================
+#       Load the query vector
+# ==================================
+print 'Retrieving local query vector...'
+    
+# Verify the specified article title is valid.
+if not query_article in title_to_idx:        
+    c.close()
+    raise('Article title "%s" does not match any in the dataset.' % query_article)
+
+# Look up the index of the vector for this article.
+query_idx = title_to_idx[query_article]
+
+# Read in the query vector.
+query_vec = h5f['lsi'][query_idx].astype('float32')
+
+# =======================================
+#       Search for similar articles
+# =======================================
+print('\nFinding most similar articles to "%s"...\n' % query_article)
+
+# Set k = 11 to get 10 results since the top result will always be the query
+# article itself.
+k = 11
+
+# Submit the query to the Nearist GPU server.    
+dists, idxs = c.query(query_vec, k=k)
+
+# Print the article titles of the ten nearest neighbors.
+# For each of the k results (omitting the top match, which
+# is the query article itself)...
+print('    %50s    Distance' % 'Title')
+print('    %50s    ========' % '=====')
+for i in range(1, k):
+    print('    %50s    %.3f' % ((id_to_title[idxs[0, i]]), dists[0, i]))
+
+# Print out the timing measurements for the query operation.
+c.print_timings()
+
+# Close the server connection.
+c.close()
